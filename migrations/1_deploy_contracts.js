e3ad6a26c618fe6ad3a1bfb2290f197953547bd2
Navneet018 <navneet@logicfirst.io>
2023-08-10 02:06:24 +0530

    changes

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,6 +1,7 @@
 const Token = artifacts.require("Token");
 module.exports = async function (deployer) {
     let acc1 = await web3.eth.getAccounts();
+    console.log(acc1,"==========")
     let feeacc = acc1[0];
     await deployer.deploy(Token);
 }
