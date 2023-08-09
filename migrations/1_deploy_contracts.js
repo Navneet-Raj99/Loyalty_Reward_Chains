eed68c68a6616fd04859d0a8f101d20364371a68
Navneet018 <navneet@logicfirst.io>
2023-08-10 02:04:16 +0530

    make token Commit

---
+++ migrations/1_deploy_contracts.js
@@ -0,0 +1,6 @@
+const Token = artifacts.require("Token");
+module.exports = async function (deployer) {
+    let acc1 = await web3.eth.getAccounts();
+    let feeacc = acc1[0];
+    await deployer.deploy(Token);
+}
